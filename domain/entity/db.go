564c474662fdd4027f2aa2448f28413a21895412
udodinho <udodinho200@gmail.com>
2023-11-29 11:03:09 +0100

    Create DB connection

---
+++ domain/entity/db.go
@@ -0,0 +1,31 @@
+package entity
+
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Config struct {
+	Host string
+	Port string
+	User string
+	Password string
+	DBName string
+	SSLMode string
+}
+
+func Connect(config *Config) (*gorm.DB, error) {
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", config.Host, config.Port, config.User, config.Password, config.DBName, config.SSLMode,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		return db, err
+	}
+
+	return db, nil
+}
