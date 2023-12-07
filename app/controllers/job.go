f0a5a18a41e2642b52801eb0fb47b20e735eac14
udodinho <udodinho200@gmail.com>
2023-12-07 16:59:45 +0100

    Add update job

--- app/controllers/job.go
+++ app/controllers/job.go
@@ -179,3 +179,100 @@ func GetJob(c *fiber.Ctx) error {
 		"data":  job,
 	})
 }
+
+func UpdateJob(c *fiber.Ctx) error {
+	now := time.Now().Unix()
+
+	claims, err := utils.ExtractTokenMetadata(c)
+
+	if err != nil {
+		return c.Status(http.StatusUnauthorized).JSON(&fiber.Map{
+			"error": true,
+			"msg":   "Token is invalid or expired",
+		})
+	}
+
+	// Set expiration time from JWT data of current job.
+	expires := claims.Expires
+
+	// Return status 401 and unauthorized error message.
+	if now > expires {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": true,
+			"msg":   "You are unauthorized, please login",
+		})
+	}
+
+	updateJob := &entity.UpdateJob{}
+
+	// Check, if received JSON data is valid.
+	err = c.BodyParser(&updateJob)
+
+	if err != nil {
+		return c.Status(http.StatusUnprocessableEntity).JSON(&fiber.Map{
+			"error": true,
+			"msg":   "Request failed",
+		})
+	}
+
+	id, err := uuid.Parse(c.Params("id"))
+
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(&fiber.Map{
+			"error": true,
+			"msg":   "Error parsing uuid",
+		})
+	}
+
+	createdBy := claims.UserID
+
+	updatedJob, db, err := entity.GetSingleJob(id, createdBy)
+
+	if id != updatedJob.ID {
+		return c.Status(http.StatusNotFound).JSON(&fiber.Map{
+			"message": "No job with the ID",
+			"data":    id})
+	}
+
+	if err != nil {
+		return c.Status(http.StatusUnprocessableEntity).JSON(&fiber.Map{
+			"error": true,
+			"msg":   "Could not fetch job",
+		})
+	}
+
+	updateJob.ID = id
+	updateJob.CreatedBy = createdBy
+	updateJob.UpdatedAt = time.Now()
+
+	// Create a new validator for a Job model.
+	validate := utils.NewValidator()
+
+	// Validate job fields.
+	if err := validate.Struct(updateJob); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(&fiber.Map{
+			"error": true,
+			"msg":   utils.ValidatorErrors(err),
+		})
+	}
+
+	if updateJob.Company != "" {
+		updatedJob.Company = updateJob.Company
+	}
+
+	if updateJob.Position != "" {
+		updatedJob.Position = updateJob.Position
+	}
+
+	if updateJob.Status != "" {
+		updatedJob.Status = updateJob.Status
+	}
+
+	db.Save(&updatedJob)
+
+	return c.Status(http.StatusOK).JSON(&fiber.Map{
+		"error": true,
+		"msg":   "Successfully updated job",
+		"data":  updatedJob,
+	})
+}
