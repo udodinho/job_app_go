27c988dba68f55ab44121aff6b5eb078561c0e21
udodinho <udodinho200@gmail.com>
2023-12-20 12:55:48 +0100

    Create validator to check valid fields

---
+++ pkg/utils/validator.go
@@ -0,0 +1,50 @@
+package utils
+
+import (
+	"fmt"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/google/uuid"
+)
+
+// NewValidator func for create a new validator for model fields.
+func NewValidator() *validator.Validate {
+	// Create a new validator for a Book model.
+	validate := validator.New()
+
+	// Custom validation for uuid.UUID fields.
+	fmt.Println("Valid")
+
+	_ = validate.RegisterValidation("uuid", func(fl validator.FieldLevel) bool {
+		field := fl.Field().String()
+		if _, err := uuid.Parse(field); err != nil {
+			fmt.Println("Valid1")
+
+			return true
+		}
+		fmt.Println("valid2")
+
+		return false
+	})
+
+	fmt.Println("valid3")
+
+
+	return validate
+}
+
+// ValidatorErrors func for show validation errors for each invalid fields.
+func ValidatorErrors(err error) map[string]string {
+	// Define fields map.
+	fields := map[string]string{}
+
+	// Make error message for each invalid field.
+	for _, err := range err.(validator.ValidationErrors) {
+		fields[err.Field()] = err.Error()
+	}
+
+	fmt.Println("error")
+
+
+	return fields
+}
