831827e01245467dcce6d30933134a52516b76e5
udodinho <udodinho200@gmail.com>
2023-12-21 14:06:38 +0100

    Create middleware

---
+++ pkg/middleware/fiber-middleware.go
@@ -0,0 +1,40 @@
+package middleware
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func FiberMiddleware(app *fiber.App) {
+	file, err := os.OpenFile("./logs.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer file.Close()
+
+	// Set config for logger
+	loggerConfig := logger.Config{
+		Output: file, // add file to save output
+	}
+
+	// Use middlewares for each route
+	app.Use(
+		logger.New(loggerConfig), // add Logger middleware with config
+	)
+	
+	// setup cors
+	app.Use(cors.New(cors.Config{
+		AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+		AllowOrigins:     "*",
+		AllowCredentials: true,
+		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+		MaxAge:           12 * int(time.Duration(time.Hour)),
+	}))
+
+}
