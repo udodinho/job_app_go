060c3fa758279c9909fcabde0f190bc6bab7d26b
udodinho <udodinho200@gmail.com>
2023-12-22 22:27:49 +0100

    Create jwt parser

---
+++ pkg/utils/jwt-parser.go
@@ -0,0 +1,80 @@
+package utils
+
+import (
+	"os"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+// TokenMetadata struct to describe metadata in JWT.
+type TokenMetadata struct {
+	UserID      uuid.UUID
+	Expires     int64
+}
+
+// ExtractTokenMetadata func to extract metadata from JWT.
+func ExtractTokenMetadata(c *fiber.Ctx) (*TokenMetadata, error) {
+	token, err := verifyToken(c)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// if err != nil {
+	// 	return nil, c.Status(http.StatusUnauthorized).JSON(&fiber.Map{
+	// 		"error": true,
+	// 		"msg":   "Unauthenticated",
+	// 	})
+	// }
+
+	// Setting and checking token and credentials.
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		// User ID.
+		userID, err := uuid.Parse(claims["id"].(string))
+		if err != nil {
+			return nil, err
+		}
+
+		// Expires time.
+		expires := int64(claims["expires"].(float64))
+
+		return &TokenMetadata{
+			UserID:      userID,
+			Expires:     expires,
+		}, nil
+	}
+
+	return nil, err
+}
+
+func extractToken(c *fiber.Ctx) string {
+	bearerToken := c.Get("Authorization")
+
+	onlyToken := strings.Split(bearerToken, " ")
+
+	if len(onlyToken) == 2 {
+		return onlyToken[1]
+	}
+
+	return ""
+}
+
+func verifyToken(c *fiber.Ctx) (*jwt.Token, error) {
+	tokenString := extractToken(c)
+
+	token, err := jwt.Parse(tokenString, jwtKeyFunc)
+	
+	if err != nil {
+		return nil, err
+	}
+
+	return token, nil
+}
+
+func jwtKeyFunc(token *jwt.Token) (interface{}, error) {
+	return []byte(os.Getenv("JWT_SECRET_KEY")), nil
+}
