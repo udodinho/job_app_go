565f07e3a62ad9148a484557fe9487e3a30dc78f
udodinho <udodinho200@gmail.com>
2023-12-25 18:38:42 +0100

    Create generate password

---
+++ pkg/utils/password-generator.go
@@ -0,0 +1,23 @@
+package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+// NormalizePassword func for returning the users input as a byte slice.
+func NormalizePassword(p string) []byte {
+	return []byte(p)
+}
+
+// GeneratePassword func for making hash & salt with user password.
+func GeneratePassword(p string) string {
+	bytePwd := NormalizePassword(p)
+
+	hash, err := bcrypt.GenerateFromPassword(bytePwd, 14)
+
+	if err != nil {
+		return err.Error()
+	}
+
+	// GenerateFromPassword returns a byte slice so we need to
+	// convert the bytes to a string and return it.
+	return string(hash)
+}
